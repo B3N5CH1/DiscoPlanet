3fa836d8fcc3274daa5d766bc3dca75fa3e82bf4
Quentin <quentin.flueckiger@hispeed.ch>
2018-05-18 08:32:35 +0200

    MainMenu C# commented

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/*
+ * This script handle the behaviour of the mainMenu.
+ */
+
 public class MainMenu : MonoBehaviour {
 
+    //Start the game with the next scene
 	public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    //Quit the game
     public void QuitGame()
     {
         Debug.Log("QUIT!");
