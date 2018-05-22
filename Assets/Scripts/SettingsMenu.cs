b0d7aff52a0d28dd12f1120af0ee86bede6103e3
Quentin <quentin.flueckiger@hispeed.ch>
2018-05-22 16:37:10 +0200

    Settings Menu
    Added sounds for StartScene and MainMenuScene. Created the options menu and implemented the volume slider.

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+    // Sets the volume of an AudioMixer
+	public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+}
